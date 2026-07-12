e92cbdd24de3a00623711a79e00032ec43b7a877
C0m3b4ck <push.decker0k@icloud.com>
2026-07-12 11:19:12 +0200

    Bug fixes and QoL improvements

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ Uses coqui-tts for realistic-sounding text-to-speech models. **NVIDIA GPU recomm
 - .doc,
 - .txt and all other raw text formats
 # To-do
+- Add chapters (one doc, multiple audio files) (preferably convert doc to .chp, which is a text file with chapter marks added)
 - Add doc-to-text conversion for all supported formats
 - Add directory conversion - all docs in dir will be turned into .txt, then synthesized into .wav
 - (optional) add worse-sounding but lighter pyttsx3 version
--- doctospeech.py
+++ doctospeech.py
@@ -1,5 +1,6 @@
 # /////////////////////////////         LIBRARY IMPORTS                 ///////////////////////////////
 import pathlib  # for relative paths
+import time # for waiting during massive text prints
 from datetime import datetime  # for timestamps
 
 import pyfiglet  # for ASCII art in greet()
@@ -12,9 +13,14 @@ from TTS.api import TTS  # for TTS functionality
 def greet():
     ASCII_art_greet = pyfiglet.figlet_format("DocToSpeech", font="alphabet")
     print(ASCII_art_greet)
-    ASCII_art_credit = pyfiglet.figlet_format("By C0m3b4ck under APL 2.0")
+    ASCII_art_credit = pyfiglet.figlet_format("By C0m3b4ck under MPL 2.0")
     print(ASCII_art_credit)
 
+def goodbye():
+    ASCII_art_bye = pyfiglet.figlet_format("Goodbye!", font='alphabet')
+    print(ASCII_art_bye)
+    ASCII_art_credit = pyfiglet.figlet_format("By C0m3b4ck under MPL 2.0")
+    print(ASCII_art_credit)
 
 def get_user_input_docs():
     txt_path = ""
@@ -45,29 +51,46 @@ def get_user_input_docs():
         txt_path = doc_path
     else:
         print("Unrecognized extention.")
-        return
 
-    while len(choice) != 1 or choice not in ("y", "n"):
-        choice = input("Treat the inputted doc as text file? y/n: ").lower()
-        if choice == "y":
-            print(
-                "File already in raw text format! Proceeding to tts option selection..."
-            )
-            txt_path = doc_path
-        elif choice == "n":
-            print("Unknown file extention. Closing program.")
-            exit()
+        while len(choice) != 1 or choice not in ("y", "n"):
+             choice = input("Treat the inputted doc as text file? y/n: ").lower()
+             if choice == "y":
+                 print("File already in raw text format! Proceeding to tts option selection...")
+                 txt_path = doc_path
+             elif choice == "n":
+                 print("Unknown file extension. Closing program.")
+                 exit()
 
     get_user_input_tts(txt_path)
 
 
 def get_user_input_tts(txt_path):
     choice = ""
+    choice_timestamp = ""
     cloning_path = ""
     use_cloning = False
     preset_speaker = ""
+    tts_initialized = False
+    tts = None
+    output_path = ""
 
-    output_file_name = input("Output file name: ")
+    output_file_name = input("Output file name (without *.wav): ").strip()
+    while (len(choice_timestamp) != 1) and choice_timestamp != "c" and choice_timestamp != "p":
+        choice_timestamp = input("Add timestamp to file name? y/n: ")
+        choice_timestamp.lower()
+        if choice_timestamp == "y":
+            # Get current datetime object
+            current_datetime = datetime.now()
+            # Convert datetime object to timestamp
+            current_timestamp = current_datetime.timestamp()
+            print("---> Current Timestamp:", current_timestamp)
+            output_path = (output_file_name + "_" + str(current_timestamp)) # .wav extention is attached automatically
+            print("---> Output file name: ", output_path)
+        elif choice_timestamp == "n":
+            output_path = output_file_name + ".wav"
+            print("---> Not adding timestamp.")
+        else:
+            print("!!! Please input 'y' or 'n' !!!")
     language = input("Language acronym (en for english): ")
 
     while (len(choice) != 1) and choice != "c" and choice != "p":
@@ -75,12 +98,14 @@ def get_user_input_tts(txt_path):
         choice.lower()
         if choice == "c":
             cloning_path = input("Input cloning voice .wav relative path: ")
-            os.path.join(os.path.dirname(__file__), cloning_path)
+            #os.path.join(os.path.dirname(__file__), cloning_path)
             use_cloning = True
         elif choice == "p":
-            print("Speakers: ", tts.speakers)
+            tts = make_tts_init(show_speakers=True)
             preset_speaker = input("Input preset speaker name: ")
             use_cloning = False
+        else:
+            print("!!! Please input 'c' or 'p' !!!")
     with open(txt_path, "r") as file:
         file_contents = file.read()
     print("File contents: ", file_contents)
@@ -93,16 +118,48 @@ def get_user_input_tts(txt_path):
         cloning_path,
         language,
         output_file_name,
+        tts,
+        output_path
     )
 
 # /////////////////////         DOCS TO TXT FUNCTIONS           //////////////////////////
 def epub_to_text(doc_path):
+    # actually implement
+    print("Not done yet!")
 def pdf_to_text(doc_path):
+    # actually implement
+    print("Not done yet!")
 def docx_to_text(doc_path):
+    # actually implement
+    print("Not done yet!")
 def doc_to_text(doc_path):
+    # actually implement
+    print("Not done yet!")
 def html_to_text(doc_path):
+    # actually implement
+    print("Not done yet!")
 def djvu_to_text(doc_path):
+    # actually implement
+    print("Not done yet!")
 # ////////////////////            TTS FUNCTIONS           /////////////////
+def make_tts_init(show_speakers): # initializes tts object
+    print("Checking for CUDA/CPU...")
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print("Using device:", device)
+
+    print("Initializing TTS model...")
+    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device) # model that supports both cloning and speakers
+
+    if show_speakers:
+        if hasattr(tts, "speakers") and tts.speakers:
+            print("Available speakers:")
+            for speaker in tts.speakers:
+                print("-", speaker)
+        else:
+            print("This model does not expose a speaker list.")
+
+    return tts
+
 def make_tts_voiceover(
     text_to_say,
     use_cloning,
@@ -110,48 +167,41 @@ def make_tts_voiceover(
     cloning_audio_relative_path,
     language,
     file_name,
+    tts,
+    output_path
 ):
-
-    # Get device
-    print("Getting device (CUDA/CPU)...")
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
-    # List available TTS models
-    # print("Available models: ",TTS().list_models())
-
-    # Initialize TTS
-    print("Initializing TTS model...")
-    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
-
+    if (tts is None):
+        tts = make_tts_init(show_speakers=False)
     # Run TTS
     # ❗ XTTS supports both, but many models allow only one of the `speaker` and
     # `speaker_wav` arguments
 
-    # Get current datetime object
-    current_datetime = datetime.now()
-
-    # Convert datetime object to timestamp
-    current_timestamp = current_datetime.timestamp()
-    print("Current Timestamp:", current_timestamp)
-    output_path = file_name + "_" + current_timestamp + ".wav"
-    print("Output file name: ", output_path)
     # TTS with list of amplitude values as output, clone the voice from `cloning_audio_relative_path`
-    if use_cloning:
-        wav = tts.tts(
-            text=text_to_say,
-            speaker_wav=cloning_audio_relative_path,
-            language=language,
-            file_path=output_path,
-        )
+    try:
+        if use_cloning:
+            tts.tts_to_file(
+                text=text_to_say,
+                speaker_wav=cloning_audio_relative_path,
+                language=language,
+                file_path=output_path,
+            )
+        else:
+            # TTS to a file, use a preset speaker
+            tts.tts_to_file(
+                text=text_to_say,
+                speaker=preset_voice,
+                language=language,
+                file_path=output_path,
+            )
+    except Exception as e:
+        print("An error occured while trying to generate file: ", e)
     else:
-        # TTS to a file, use a preset speaker
-        tts.tts_to_file(
-            text=text_to_say,
-            speaker=preset_voice,
-            language=language,
-            file_path=output_path,
-        )
-
+        print("~~~ File made successfully!!! ~~~")
+        print("Output file path: ", output_path)
+        print("Language: ", language)
+        time.sleep(1)
+        print("Text read: ", text_to_say)
+        goodbye()
 
 # /////////////////////////////////                 MAIN FUNCTION - ENTRY POINT                     //////////////////////////////
 if __name__ == "__main__":
